2059ae83e83d33d56d5aefb0edabc7225ebead6e
KasraMg <kasragithub7@gmail.com>
2026-08-31 16:22:29 +0330

    feat(project-member): create new endpoint for remove user from project

--- src/project-members/project-members.controller.ts
+++ src/project-members/project-members.controller.ts
@@ -6,6 +6,7 @@ import {
   UseGuards,
   Req,
   ParseIntPipe,
+  Delete,
 } from '@nestjs/common';
 import { ProjectMembersService } from './project-members.service';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth/jwt-auth.guard';
@@ -34,6 +35,29 @@ export class ProjectMembersController {
   ) {
     return this.projectMembersService.findProjectMembers(
       projectId,
+      req.user as User,
+    );
+  }
+
+  @Delete(':projectId/:userId')
+  @UseGuards(JwtAuthGuard)
+  @ApiParam({
+    name: 'projectId',
+    type: Number,
+  })
+  @ApiParam({
+    name: 'userId',
+    type: Number,
+  })
+  @ApiBearerAuth()
+  removeUser(
+    @Param('projectId', ParseIntPipe) projectId: number,
+    @Param('userId', ParseIntPipe) userId: number,
+    @Req() req: Express.Request,
+  ) {
+    return this.projectMembersService.removeUser(
+      projectId,
+      userId,
       req.user as User,
     );
   }
--- src/project-members/project-members.service.ts
+++ src/project-members/project-members.service.ts
@@ -1,6 +1,14 @@
-import { ForbiddenException, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { ProjectMember } from './entities/project-member.entity';
+import {
+  ProjectMember,
+  ProjectMemberRole,
+} from './entities/project-member.entity';
 import { Repository } from 'typeorm';
 import { User } from 'src/users/entities/user.entity';
 
@@ -11,6 +19,42 @@ export class ProjectMembersService {
     private projectMemberRepository: Repository<ProjectMember>,
   ) {}
 
+  async removeUser(projectId: number, userId: number, user: User) {
+    const projectMember = await this.projectMemberRepository.findOne({
+      where: {
+        project: {
+          id: projectId,
+        },
+        user: {
+          id: userId,
+        },
+      },
+      relations: {
+        project: {
+          owner: true,
+        },
+      },
+    });
+
+    if (!projectMember) {
+      throw new NotFoundException('member not found');
+    }
+    if (projectMember.project.owner.id !== user.id) {
+      throw new ConflictException(
+        'You are not allowed to remove project users',
+      );
+    }
+
+    if (projectMember.role == ProjectMemberRole.OWNER) {
+      throw new ConflictException('owner cannot be removed');
+    }
+
+    await this.projectMemberRepository.delete(projectMember.id);
+    return {
+      success: true,
+      message: 'User removed successfully',
+    };
+  }
   findAll() {
     return this.projectMemberRepository.find();
   }
